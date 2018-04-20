9aca2c8f06613b819e418162e48cb319a0089891
Mateusz Kotowski <kotas55@wp.pl>
2018-04-20 14:17:15 +0200

    start projektu

---
+++ src/main/java/pl/coderslab/AppInitializer.java
@@ -0,0 +1,29 @@
+package pl.coderslab;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+public	class	AppInitializer	extends
+        AbstractAnnotationConfigDispatcherServletInitializer	{
+
+    @Override
+    protected	Class<?>[]	getRootConfigClasses()	{
+        return	null;
+    }
+
+    @Override
+    protected	Class<?>[]	getServletConfigClasses()	{
+        return	new	Class[]{AppConfig.class};
+    }
+
+    @Override
+    protected	String[]	getServletMappings()	{
+        return	new	String[]{"/"};
+    }
+}
